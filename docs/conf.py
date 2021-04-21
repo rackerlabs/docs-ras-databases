134cd47238d18279c877bb0d05e50f1d70cd7902
Ravi <raviteja.valluri@rackspace.com>
2021-04-21 07:32:07 -0700

    Scripts and Conf Updates

--- docs/conf.py
+++ docs/conf.py
@@ -209,7 +209,8 @@ else:
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "collapse_navigation" : False
+    "collapse_navigation" : False,
+    "sticky_navigation": True, 
 }
 
 
